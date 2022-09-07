7311d09ba828a71c4c81af74b9a8731a5fa6db24
dbdnrl123 <dbdnrl123@gmail.com>
2022-09-07 19:42:00 +0900

    수행평가 별그리기

---
+++ Test/src/Test1/_01.java
@@ -0,0 +1,21 @@
+package Test1;
+
+import java.util.Scanner;
+
+public class _01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
