94543e8221ab89ea41613e51c3f9a97be39e564d
dbdnrl123 <dbdnrl123@gmail.com>
2022-09-14 19:48:15 +0900

    2022/09/14 수업 예제

---
+++ Ch10/src/sub3/Apple.java
@@ -0,0 +1,21 @@
+package sub3;
+
+import java.io.Serializable;
+
+public class Apple implements Serializable{
+
+	private String country;
+	private int price;
+
+	public Apple(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+	
+	public void show() {
+		// 객체 정보 조회
+		System.out.println("원산지 : "+ country);
+		System.out.println("가격 : "+ price);
+		
+	}
+}
---
+++ Ch10/src/sub3/DeserializeTest.java
@@ -0,0 +1,36 @@
+package sub3;
+/*
+ * 날짜 : 2022/09/14
+ * 이름 : 양재영
+ * 내용 : 역직렬화 실습하기
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+
+public class DeserializeTest {
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\java2\\Desktop\\Apple.dat";
+		
+		try {
+			FileInputStream fis = new FileInputStream(path);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			// 객체 가져오기
+			Apple apple = (Apple) ois.readObject();
+			
+			// 스트림 해제
+			ois.close();
+			fis.close();
+			
+			apple.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("역직렬화 완료...");
+	}
+
+}
