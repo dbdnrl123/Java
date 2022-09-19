ce807defbfc34bb6470c9516b07bea1c8d579c7a
dbdnrl123 <dbdnrl123@gmail.com>
2022-09-19 19:06:06 +0900

    2022/09/19 수업예제

---
+++ Ch12/src/sub2/ThreadJoinTest.java
@@ -0,0 +1,30 @@
+package sub2;
+/*
+ * 날짜 : 2022/09/19
+ * 이름 : 양재영
+ * 내용 : 스레드 Join 실습하기
+ */
+
+public class ThreadJoinTest {
+	public static void main(String[] args) throws InterruptedException {
+		
+		Count count = new Count();
+	
+		CountThread ct1 = new CountThread(count);
+		CountThread ct2 = new CountThread(count);
+		CountThread ct3 = new CountThread(count);
+		
+		ct1.start();
+		ct2.start();
+		ct3.start();
+		
+		// 자식스레드의 작업이 완료된후 다시 메인스레드로 합류(스레드 대기)
+		ct1.join();
+		ct2.join();
+		ct3.join();
+		
+		System.out.println("count 값 : " + count.getNum());
+
+	}
+
+}
